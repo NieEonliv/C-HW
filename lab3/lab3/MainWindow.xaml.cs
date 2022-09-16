e6f1535a928e51c0d69777bf5f8172ba9203825d
NieEonliv <63141394+NieEonliv@users.noreply.github.com>
2022-09-16 14:22:50 +0300

    New lab3

---
+++ lab3/lab3/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();          
+        }
+        private int GetElement(int i, int j)
+        {
+            if (i == 1 || j ==1)
+                return 1;
+            else
+                return GetElement(i, j-1) + GetElement(i-1, j);            
+        }
+
+        private void GetElement_Clilk(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(iValue.Text , out int resi) || !int.TryParse(jValue.Text, out int resj))
+                return;           
+            result.Text = GetElement(resi, resj).ToString();
+        }
+    }
+}

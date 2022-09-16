74c2ce20a36973a6b3b0d0c3c050e0b3e2d13170
NieEonliv <63141394+NieEonliv@users.noreply.github.com>
2022-09-16 21:27:12 +0300

    new lab 6

---
+++ lab6/lab1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private bool checkNullInput()
+        {
+            if (startCoordinate.Text == "" || endCoordinate.Text == "" || stepCoordinate.Text == "")
+            {
+                MessageBox.Show("Введите коректрые данные");
+                return true;
+            }
+            else
+                return false;       
+        }
+        private double[] BrushGraphikAndReturnDataY()
+        {
+            List<double[]> coordinates = NumericalIntegration.GetGraphikCoordinate(double.Parse(startCoordinate.Text),
+                                                              double.Parse(endCoordinate.Text),
+                                                              double.Parse(stepCoordinate.Text));
+            WpfPlot.Plot.AddScatter(coordinates[0], coordinates[1]);
+            WpfPlot.Refresh();
+            return coordinates[1];
+        }
+       
+        private void TrapezoidMethod_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkNullInput())
+                return;
+            double[] dataY = BrushGraphikAndReturnDataY();
+            resultCalculate.Text = "Ответ: " + NumericalIntegration.Trapezoid(dataY, double.Parse(stepCoordinate.Text)).ToString();
+        }
+    }
+}
---
+++ lab6/lab1/NumericalIntegration.cs
@@ -0,0 +1,69 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public static class NumericalIntegration
+    {
+        public static double GausMethod(double a, double b)
+        {
+            double tempLeftOperand = (a + b) / 2d;
+            double tempRightOperand = (b - a) / (2 * Math.Sqrt(3d));
+
+            double f0 = CalculateFunction(tempLeftOperand - tempRightOperand);
+            double f1 = CalculateFunction(tempLeftOperand + tempRightOperand);
+
+            double resultCalculate = ((b - a) / 2d) * (f0 + f1);
+            return resultCalculate;
+        }
+        
+
+        public static double Trapezoid(double[] dataY, double step)
+        {
+            double sumTemp = 0;
+            for (int i = 1; i < dataY.Length - 1; i++)
+                sumTemp += dataY[i];
+
+            double resultCalculate = dataY[0] + dataY[dataY.Length - 1] / 2d;
+            resultCalculate = (sumTemp + resultCalculate) * step;
+
+            return resultCalculate;
+        }
+        
+        public static double CalculateFunction(double value)
+        {
+            double degreeValue4 = Math.Pow(value, 4);
+            double degreeValue2 = 3d * Math.Pow(value, 2);
+            double znam = degreeValue4 + degreeValue2 + 2d;
+            return value / znam;
+        }
+        public static List<double[]> GetGraphikCoordinate(double startCoordinate, double endCoordinate, double step)
+        {
+            List<double[]> coordinate = new List<double[]>();
+
+            double difference = (endCoordinate - startCoordinate) + 1;
+            double tempCountOperations = difference / step;
+            int countOperations = (int)Math.Round(tempCountOperations, MidpointRounding.AwayFromZero);
+
+            double[] dataX = new double[countOperations];
+            double[] dataY = new double[countOperations];
+
+            for (int i = 0; i < countOperations; i++)
+            {
+                dataX[i] = startCoordinate;
+                startCoordinate += step;
+
+                dataY[i] = CalculateFunction(dataX[i]);
+            }
+
+            coordinate.Add(dataX);
+            coordinate.Add(dataY);
+
+            return coordinate;
+        }
+    }
+}
